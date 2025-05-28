bbf4f61efa25565fa40284927e0a3fe0bfcb5e5f
Martin Foltýn <martin@foltyn.dev>
2025-05-28 21:06:19 +0200

    logger: prepare for test

--- src/logger.ts
+++ src/logger.ts
@@ -1,5 +1,5 @@
 import log from 'loglevel';
-import type { LogLevelDesc } from 'loglevel';
+import type { LogLevelNames, LogLevelDesc } from 'loglevel';
 import minimist from 'minimist';
 
 const args = minimist(process.argv.slice(2));
@@ -13,6 +13,22 @@ if (args.vv) {
     level = 'debug';
 }
 
-log.setLevel(level);
+// Store the original factory
+// const originalFactory = log.methodFactory;
+
+// log.methodFactory = (methodName: LogLevelNames, logLevel: log.LogLevelNumbers, loggerName?: string | symbol) => {
+//     const rawMethod = originalFactory(methodName, logLevel, loggerName as string);
+
+//     // Redirect info, debug, and trace to stderr. Warn and error already go to stderr by default.
+//     if (methodName === 'info' || methodName === 'debug' || methodName === 'trace') {
+//         return (...messages: any[]) => {
+//             console.error(...messages);
+//         };
+//     }
+//     // For 'warn' and 'error', use the original method which directs to stderr.
+//     return rawMethod;
+// };
+
+log.setLevel(level); // Apply the new factory and level
 
 export default log; 
