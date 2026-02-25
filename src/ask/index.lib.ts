bb1969166135a22c548132786c4547a0c2568d66
Martin <admin@phpcode.eu>
2026-02-25 22:58:03 +0100

    feat(ask): add barrel export for AIChat library

---
+++ src/ask/index.lib.ts
@@ -0,0 +1,18 @@
+export { AIChat } from "./AIChat";
+export { ChatEvent } from "./lib/ChatEvent";
+export { ChatSession } from "./lib/ChatSession";
+export { ChatSessionManager } from "./lib/ChatSessionManager";
+export { ChatLog } from "./lib/ChatLog";
+export { ChatTurn } from "./lib/ChatTurn";
+export type {
+    AIChatOptions,
+    AIChatTool,
+    AIChatSelection,
+    SendOptions,
+    ChatResponse,
+    ToolCallResult,
+    SessionEntry,
+    LogEntry,
+    LogLevel,
+    SessionStats,
+} from "./lib/types";
