0453896bf2776a29432b718cf3ab9d97ef4bb65f
Martin <admin@phpcode.eu>
2026-05-08 20:37:32 +0200

    feat(utils/WebView): add index re-exports

---
+++ src/utils/WebView/index.ts
@@ -0,0 +1,19 @@
+export { WebView } from "./WebView";
+// WebViewPool added in Task 7
+export {
+    WebViewError,
+    WebViewEvaluateError,
+    WebViewNavigationError,
+    WebViewTimeoutError,
+} from "./errors";
+export type {
+    DataStoreConfig,
+    EvaluateOptions,
+    NavigateOptions,
+    ScreenshotEncoding,
+    ScreenshotFormat,
+    ScreenshotOptions,
+    ScreenshotResult,
+    WebViewBackend,
+    WebViewOptions,
+} from "./types";
